260c9447bcc26bf545b5ae31dd7c37dbe2e4b432
brian <bcburnett@gmail.com>
2019-06-13 23:00:57 -0400

    Initial commit

---
+++ src/main/java/gradleproject1/Letters.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gradleproject1;
+
+/**
+ *
+ * @author brian
+ */
+public class Letters {
+
+  public String letter;
+  public boolean correct = false;
+
+  public Letters(String s) {
+    letter = s;
+    if (s.matches("[ .'?!,&-]")) correct = true;
+  }
+
+  public Letters(String s, boolean b) {
+    letter = s;
+    correct = b;
+  }
+}
---
+++ src/main/java/gradleproject1/ReadLine.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gradleproject1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author brian
+ */
+public class ReadLine {
+
+    public String readLine() {
+        Scanner input = new Scanner(System.in);
+        return input.nextLine();
+    }
+
+}
